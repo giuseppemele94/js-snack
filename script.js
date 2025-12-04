75ffec882e84056928c4e71921054195f3b57a90
Giuseppe Mele <giuseppemele9410@gmail.com>
2025-12-04 12:24:59 +0100

    feat : exercise 2

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
  L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore
 */
 
-const number1 = parseInt(prompt("Inserisci il primo numero:")); 
+/*const number1 = parseInt(prompt("Inserisci il primo numero:")); 
 const number2 = parseInt(prompt("Inserisci il secondo numero:")); 
 
 
@@ -17,6 +17,7 @@ else {
   console.log("i numeri sono uguali "); 
 }
   
+//Versione alternativa con Math 
 /*
 if(number1 === number2) {
   console.log("I numeri sono uguali. ");
@@ -25,4 +26,34 @@ else {
   const max = Math.max(number1, number2); 
   console.log("Maggiore:" + max); 
 }
+  */
+
+
+/*JSnack 3
+ Il software deve chiedere per 5 volte all’utente di inserire un numero. 
+ Il programma stampa la somma di tutti i numeri inseriti. 
+ Esegui questo programma in due versioni, con il for e con il while (facoltativo)*/
+
+ let sum = 0; 
+ 
+ //con ciclo for 
+ for (let i = 0 ; i < 5; ++i ) {
+  const number = parseInt(prompt("Inserisci il numero :")); 
+  sum += number;
+ }
+ console.log("la somma dei numeri inseriti è :" ,sum); 
+
+
+
+/*
+//ciclo while 
+ let counter = 0;
+ let sum = 0; 
+ while ( counter < 5 ) {
+  const number = parseInt(prompt("Inserisci il numero :")); 
+  sum+= number ; 
+  ++counter; 
+  
+ }
+ console.log("Somma :" , sum); 
   */
